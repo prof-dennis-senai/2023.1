7f88ab75909e2cc33c71ef05c621e8fee6521ebe
Dennis Rocha <dennis0201@gmail.com>
2024-09-02 22:07:54 -0300

    Aula: 02/07/2024

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from myapp.views import home, sobre, contato
+from myapp.views import home, sobre, contato, usuarios
 
 urlpatterns = [
     path('', home),
     path('sobre/', sobre),
-    path('contato/', contato)
+    path('contato/', contato),
+    path('usuarios/', usuarios),
+    path('usuarios/<int:id>/', usuarios),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 
+data = [
+    {"nome": "Joaquim"},
+]
 # Create your views here.
 def home(request):
-    return render(request, 'myapp/globals/home.html')
+    nome = request.POST.get('nome')
+    if nome:
+        data.append({"nome": nome}) 
+    return render(request, 'myapp/globals/home.html',{"dados":data,"ultimo_nome":nome},status=500)
 
 def sobre(request):
     return render(request, 'myapp/globals/sobre.html')
 
 def contato(request):
     return render(request, 'myapp/globals/contato.html')
+
+
+def usuarios(request,id=None):
+    try:
+        nome = data[id-1]
+    except:
+        nome = None
+    return render(request, 'myapp/globals/usuarios.html',nome)
